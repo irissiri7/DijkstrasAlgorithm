b231fc223f5da64525d3ac3a3ccf4a47098c8b68
irissiri7 <48020183+irissiri7@users.noreply.github.com>
2020-04-13 20:20:27 +0200

    Set up the node class

---
+++ Dijkstras/Classes/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    public class Node
+    {
+        public string Name { get; private set; }
+        public int DistanceFromStart { get; private set; }
+        public List<NodeConnection> Connections { get; private set; }
+
+        public Node(string name)
+        {
+            Name = name;
+        }
+
+        public void AddConnection(Node node, int distance)
+        {
+
+        }
+    }
+}
