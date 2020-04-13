032673013b2ec5bedec20416aeae51904768dd9d
irissiri7 <48020183+irissiri7@users.noreply.github.com>
2020-04-13 20:39:07 +0200

    Made the Graph class

---
+++ Dijkstras/Classes/Graph.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    public class Graph
+    {
+        public Dictionary<string, Node> Nodes { get; private set; }
+
+        public Graph()
+        {
+            Nodes = new Dictionary<string, Node>();
+        }
+
+        public void AddNode(string name)
+        {
+            Node node = new Node(name);
+            Nodes.Add(name, node);
+        }
+
+        public void AddConnection(string fromNode, string toNode, int distance)
+        {
+            if (Nodes.ContainsKey(fromNode) && Nodes.ContainsKey(toNode))
+            {
+                Nodes[fromNode].AddConnection(Nodes[toNode], distance);
+            }
+            else
+            {
+                throw new ArgumentException("One or both of the nodes do not exist");
+            }
+        }
+    }
+}
